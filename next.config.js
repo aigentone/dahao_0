b26649cccf62da33182f31c1f43e83cd3a996241
aigentone <aigentone@icloud.com>
2025-06-09 13:09:13 -0400

    add

--- next.config.js
+++ next.config.js
@@ -2,4 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'standalone',
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
 }
+
+module.exports = nextConfig
